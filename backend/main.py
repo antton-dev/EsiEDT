b6f07222fb1b9324f17b4306ba6dfae9ef583abe
antton-dev <antton.chevrier@etik.com>
2026-08-28 16:04:07 +0200

    Cache Duration fix

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ CATEGORY_ORDER = [
 SHORT_NAME_PREFIX_ORDER = ["TP", "Gr", "Pr"]
 
 LAST_GOOD_RESPONSE: dict[str, dict] = {}
-CACHE_DURATION_SECONDS = 10
+CACHE_DURATION_SECONDS = 7200
 
 SIMULATE_ADE_DOWN = False
 
@@ -139,20 +139,20 @@ async def get_schedule(resource_id: str):
         return cached['data']
 
     # ICS local
-    if FIXTURE_MODE:
-        with open(FIXTURE_ICS_PATH, "rb") as f:
-            ics_content = f.read()
+    # if FIXTURE_MODE:
+    #     with open(FIXTURE_ICS_PATH, "rb") as f:
+    #         ics_content = f.read()
 
-        structured_data = parse_ics(ics_content)
-        result = {
-            "status": "success",
-            "resource_id": resource_id,
-            "total_events": len(structured_data),
-            "fetched_at": datetime.fromtimestamp(now, tz=timezone.utc).isoformat(),
-            "events": structured_data
-        }
-        LAST_GOOD_RESPONSE[resource_id] = {"data": result, "fetched_at": now}
-        return result
+    #     structured_data = parse_ics(ics_content)
+    #     result = {
+    #         "status": "success",
+    #         "resource_id": resource_id,
+    #         "total_events": len(structured_data),
+    #         "fetched_at": datetime.fromtimestamp(now, tz=timezone.utc).isoformat(),
+    #         "events": structured_data
+    #     }
+    #     LAST_GOOD_RESPONSE[resource_id] = {"data": result, "fetched_at": now}
+    #     return result
 
 
     month = datetime.now().month
